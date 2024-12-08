0e82f6db53143e4c4b9dbeec08a8893d3c9e7758
gopi2401 <gobinath@streanflo.io>
2024-12-08 11:29:19 +0530

    update shell

--- setup.sh
+++ setup.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 echo "Hello World!"
+
+# apt install nodejs
+apt-get install unzip
+curl -L -o restaurant-backend-test.zip https://github.com/gopi2401/restaurant-backend/archive/refs/tags/test.zip
+unzip restaurant-backend-test.zip.zip -d ./
+cd restaurant-backend-test.zip
