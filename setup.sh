a3dbf275d2f5e587fba2081e8bd9e9e974f37b2d
gopi2401 <gobinath@streanflo.io>
2024-12-12 14:32:48 +0530

    update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@
 
 {
     green='\033[0;32m'
+    Red='\033[0;31m'
     nc='\033[0m' # No Color
     # title design
     command printf "${green}************************************\n"
@@ -9,13 +10,17 @@
     command printf "************************************${nc}\n"
     command pkg update && pkg upgrade
     command pkg install -y git nodejs
-    # mysql(){
-    #     command
-    # }
+
+    mysql_setup() {
+        command pkg install mariadb -y
+        command mysql_install_db                 # Initialize the MariaDB data directory
+        command mysqld_safe &                    # Start the MariaDB server
+        command echo "mysqld_safe &" >>~/.bashrc # add the startup command
+    }
 
     project_setup() {
-        command git clone https://github.com/gopi401/restaurant-backend || {
-            command printf 'Failed to clone project repo. Please report this!'
+        command git clone https://github.com/gopi2401/restaurant-backend || {
+            command printf "${Red}Failed:${nc}clone project repo. Please report this!\n"
             exit 2
         }
     }
