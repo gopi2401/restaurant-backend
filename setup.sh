c29bc5497102a87cce3efb061e02d87d6da1a7f0
gopi2401 <gobinath@streanflo.io>
2024-12-22 21:57:04 +0530

    fix: something

--- setup.sh
+++ setup.sh
@@ -9,7 +9,7 @@
     command printf "*         Backend Setup            *\n"
     command printf "************************************${nc}\n"
     command pkg update && pkg upgrade
-    command pkg install -y git nodejs
+    # command pkg install -y git nodejs
 
     pkg_install() {
         pkg_list=("git" "nodejs")
@@ -22,27 +22,27 @@
         done
     }
 
-    mysql_setup() {
-        command pkg install mariadb -y
-        command mysql_install_db                 # Initialize the MariaDB data directory
-        command mysqld_safe &                    # Start the MariaDB server
-        command echo "mysqld_safe &" >>~/.bashrc # add the startup command
-        command mysql -u root -e "create database restaurant"
-        command curl -o restaurant.sql https://raw.githubusercontent.com/gopi2401/restaurant-backend/main/restaurant.sql || {
-            command printf "${Red}Failed:${nc}Download database. Please report this!\n"
-            exit 2
-        }
-        command mysql mysql <restaurant.sql >mysqllogs.log &&
-            echo "New password was successfully set." ||
-            echo "Failed to set new password"
-        command rm restaurant.sql
-        if [ ! -s mysqllogs.log ]; then
-            command rm mysqllogs.log
-        fi
-    }
+    # mysql_setup() {
+    #     command pkg install mariadb -y
+    #     command mysql_install_db                 # Initialize the MariaDB data directory
+    #     command mysqld_safe &                    # Start the MariaDB server
+    #     command echo "mysqld_safe &" >>~/.bashrc # add the startup command
+    #     command mysql -u root -e "create database restaurant"
+    #     command curl -o restaurant.sql https://raw.githubusercontent.com/gopi2401/restaurant-backend/main/restaurant.sql || {
+    #         command printf "${Red}Failed:${nc}Download database. Please report this!\n"
+    #         exit 2
+    #     }
+    #     command mysql mysql <restaurant.sql >mysqllogs.log &&
+    #         echo "New password was successfully set." ||
+    #         echo "Failed to set new password"
+    #     command rm restaurant.sql
+    #     if [ ! -s mysqllogs.log ]; then
+    #         command rm mysqllogs.log
+    #     fi
+    # }
 
     project_setup() {
-        command git clone https://github.com/gopi2401/restaurant-backend || {
+        command git clone https://github.com/gopi2401/restaurant-backend.git || {
             command printf "${Red}Failed:${nc}clone project repo. Please report this!\n"
             exit 2
         }
@@ -76,6 +76,7 @@
         fi
     }
 
+    pkg_install
     project_setup
     project_env_file
 }
